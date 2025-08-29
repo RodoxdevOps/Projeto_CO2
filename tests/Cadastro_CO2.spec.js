08093125d18e8dcd941469a517637e3c69bb44e3
RodoxdevOps <rodolphogasper@gmail.com>
2025-08-29 16:29:35 -0300

    Nova melhoria código do playwright

--- tests/Cadastro_CO2.spec.js
+++ tests/Cadastro_CO2.spec.js
@@ -2,7 +2,8 @@
 import { test, expect } from '@playwright/test';
 
 test('Acesso a página', async ({ page }) => {
-  await page.goto('https://co2-qa.brado.com.br/');
+  await page.goto('/');
+test('Cadastro de Solicitação', async ({ page }) => {
   await page.getByRole('button', { name: 'Calcular emissão' }).click();
   await page.getByRole('combobox', { name: 'Tipo de Carga' }).locator('span').click();
   await page.getByText('Algodão e derivados').click();
@@ -17,6 +18,8 @@ test('Acesso a página', async ({ page }) => {
   await page.getByRole('combobox', { name: 'Cidade Destino' }).fill('sumare');
   await page.getByText('Sumaré - SP').click();
   await page.getByRole('button', { name: 'Avançar' }).click();
+  });
+test('Cadastro Dados Pessoais', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Nome' }).click();
   await page.getByRole('textbox', { name: 'Nome' }).fill('Rodolpho Alberto Magalhaes');
   await page.getByRole('textbox', { name: 'Empresa' }).click();
@@ -27,8 +30,9 @@ test('Acesso a página', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Telefone' }).fill('41997995522');
   await page.getByRole('textbox', { name: 'E-mail' }).click();
   await page.getByRole('textbox', { name: 'E-mail' }).fill('tr.rmagalhaes@brado.com.br');
-  //await page.screenshot({ path: 'screenshot_Email.png' }); (para tirar print da tela )
+  await page.screenshot({ path: 'screenshot_Email.png' }); //(para tirar print da tela )
   //await page.getByRole('button', { name: 'Avançar' }).click();
   //await page.getByRole('button', { name: 'fechar' }).click();
+  });
 
 });
